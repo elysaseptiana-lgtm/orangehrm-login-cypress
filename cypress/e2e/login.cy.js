98f4cf1fb3df64a0d0849bdb047f46e35831df1d
Elysa Septiana <elysaseptiana@gmail.com>
2026-08-16 00:06:55 +0700

    first commit

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,9 +1,9 @@
 describe('OrangeHRM Login', () => {
-  beforeEach (() => {
+  beforeEach(() => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
   })
 
-  it('TC_01 - User able to open login page', () => {
+  it('TC_01 - User is able to open login page', () => {
 
     cy.url().should('include', '/auth/login')
     cy.contains('Login').should('be.visible')
@@ -13,7 +13,7 @@ describe('OrangeHRM Login', () => {
 
   })
 
-  it('TC_02 - User able to login with valid username and password', () => {
+  it('TC_02 - User is able to login with valid username and password', () => {
 
     cy.get('input[name="username"]').type('Admin')
     cy.get('input[name="password"]').type('admin123')
@@ -23,7 +23,7 @@ describe('OrangeHRM Login', () => {
 
   })
 
-  it('TC_03 - Users unable to login when the username field is left blank', () => {
+  it('TC_03 - User is unable to login when the username field is left blank', () => {
     
     cy.get('input[name="password"]').type('admin123')
     cy.get('button[type="submit"]').click()
@@ -31,7 +31,7 @@ describe('OrangeHRM Login', () => {
      
   })
 
-  it('TC_04 - User unable to login when password field is left blank', () => {
+  it('TC_04 - User is unable to login when password field is left blank', () => {
     
     cy.get('input[name="username"]').type('Admin')
     cy.get('button[type="submit"]').click()
@@ -39,14 +39,14 @@ describe('OrangeHRM Login', () => {
 
   })
 
-  it('TC_05 - User unable to login when username and password fields are left blank', () => {
+  it('TC_05 - User is unable to login when username and password fields are left blank', () => {
 
     cy.get('button[type="submit"]').click()
     cy.contains('Required').should('be.visible')
 
   })
 
-  it('TC_06 - User login with invalid username', () => {
+  it('TC_06 - User is login with invalid username', () => {
     cy.get('input[name="username"]').type('Wrong User')
     cy.get('input[name="password"]').type('admin123')
     cy.get('button[type="submit"]').click()
@@ -54,7 +54,7 @@ describe('OrangeHRM Login', () => {
 
   })
 
-  it('TC_07 - User login with invalid password', () => {
+  it('TC_07 - User is login with invalid password', () => {
 
     cy.get('input[name="username"]').type('Admin')
     cy.get('input[name="password"]').type('ngasal123')
@@ -63,9 +63,9 @@ describe('OrangeHRM Login', () => {
 
   })
 
-  it('TC_08 - User login with invalid username and password', () => {
+  it('TC_08 - User is login with invalid username and password', () => {
 
-    cy.get('input[name="username"]').type('Wrong USer')
+    cy.get('input[name="username"]').type('Wrong User')
     cy.get('input[name="password"]').type('ngasal123')
     cy.get('button[type="submit"]').click()
     cy.contains('Invalid credentials').should('be.visible')
@@ -75,31 +75,31 @@ describe('OrangeHRM Login', () => {
   it('TC_09 - User login is case-sensitive regarding the username (lowercase)', () => {
 
     cy.get('input[name="username"]').type('admin')
-    cy.get('input[name="password"]').type('adminl123')
+    cy.get('input[name="password"]').type('admin123')
     cy.get('button[type="submit"]').click()
     cy.contains('Invalid credentials').should('be.visible')
 
   })
 
-  it('TC_010 - User login is case-sensitive regarding the username (uppercase)', () => {
+  it('TC_10 - User login is case-sensitive regarding the username (uppercase)', () => {
 
     cy.get('input[name="username"]').type('ADMIN')
-    cy.get('input[name="password"]').type('adminl123')
+    cy.get('input[name="password"]').type('admin123')
     cy.get('button[type="submit"]').click()
     cy.contains('Invalid credentials').should('be.visible')
 
   })
 
-  it('TC_011 - User login with very long text', () => {
+  it('TC_11 - User is login with very long text', () => {
 
     cy.get('input[name="username"]').type('abcdefghijklmnopqrstuvwxyzqwertyuiopasdfghjklzxcvbnm')
-    cy.get('input[name="password"]').type('adminl23')
+    cy.get('input[name="password"]').type('admin123')
     cy.get('button[type="submit"]').click()
     cy.contains('Invalid credentials').should('be.visible')
 
   })
 
-  it('TC_012 - User able to access "Forgot your password?"', () => {
+  it('TC_12 - User is able to access "Forgot your password?"', () => {
 
     cy.contains('Forgot your password?').click()
     cy.url().should('include', '/auth/requestPasswordResetCode')
@@ -110,4 +110,3 @@ describe('OrangeHRM Login', () => {
 
 
 })
-
